7f337c8b83b3aaf6e82620236974ef015bc171a1
Jose Ramírez <98234695+jramicr@users.noreply.github.com>
2023-06-06 18:12:39 -0600

    Tarea incompleta

---
+++ app/src/main/java/com/gquesada/layouts/ConstraintFormActivity.kt
@@ -0,0 +1,11 @@
+package com.gquesada.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_form)
+    }
+}
